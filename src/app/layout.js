50035766fdffde0d11fe94a892a3a291a02666c5
jvsobraz <jvsobraz@hotmail.com>
2023-09-26 16:23:00 -0300

    jwt

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,23 @@
 import { Toaster } from 'react-hot-toast'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { AuthProvider } from '@/contexts/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'DigitalBusinessEnablement',
-  description: 'Um app de controle de finanças pessoais',
+  title: 'CarSearch',
+  description: 'Um app de controle de vendas e compras de veículos.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>
-        {children}
-        <Toaster position="bottom-right" />
+        <AuthProvider>
+          {children}
+          <Toaster position="bottom-right" />
+        </AuthProvider>
       </body>
     </html>
   )
--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,8 +1,8 @@
-export default function TextField({label, id, ...props}){
+export default function TextField({label, id, name, register=()=>{}, ...props}){
     return (
         <div className="flex flex-col gap-1">
             <label htmlFor={id} className="text-slate-100">{label}</label>
-            <input type="text" id={id} {...props} className="bg-slate-800 rounded px-3 py-1 outline-none focus:ring-1 focus:ring-pink-600"  />
+            <input {...register(name)} type="text" id={id} {...props} className="bg-slate-800 rounded px-3 py-1 outline-none focus:ring-1 focus:ring-pink-600"  />
         </div>
 
     )
